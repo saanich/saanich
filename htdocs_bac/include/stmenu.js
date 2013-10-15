812fe8346ea098dc7cdd35dfdc974f8399e79fef
saanich <joel.friesen@saanich.ca>
2013-10-15 08:29:14 -0700

    Removed

--- htdocs_bac/include/stmenu.js
+++
@@ -1,2 +0,0 @@
-//used by Property Profile Report, http://saanich.ca/webapp/ppr/
-document.write("<script src='/stmenu.js'><\/script>");
--- htdocs_bac/ui/scripts/sitemap.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-  $file = $_SERVER['DOCUMENT_ROOT'].'/navigation/'.$_GET["f"];
-
-  if (file_exists($file)) {
-    echo str_replace('href="../', 'href="/', file_get_contents($file));
-  }
-  
-?>
